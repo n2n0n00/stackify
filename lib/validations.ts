bb3083c19ef1554979e593de6806719ebad15f9a
AT <athinat690@gmail.com>
2023-10-13 13:01:16 +0100

    ask quesiton form

---
+++ lib/validations.ts
@@ -0,0 +1,9 @@
+import z from "zod";
+
+// Define what goes into the form
+// tags = min of 1 tag and max of 3 and each must be min of 1 letters and max of 15
+export const QuestionsSchema = z.object({
+  title: z.string().min(5).max(130),
+  explanation: z.string().min(100),
+  tags: z.array(z.string().min(1).max(15)).min(1).max(3),
+});
