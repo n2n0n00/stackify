c7f78fcd172742da131f6029dfdb9a2b84aaeb29
AT <athinat690@gmail.com>
2023-10-17 20:32:06 +0100

    userid fix

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,20 +1,21 @@
-import Question from "@/components/forms/Question";
-import { getUserById } from "@/lib/actions/user.action";
-import { auth } from "@clerk/nextjs";
-import { redirect } from "next/navigation";
 import React from "react";
+import Question from "@/components/forms/Question";
+// import { auth } from "@clerk/nextjs";
+import { getUserById } from "@/lib/actions/user.action";
+import { redirect } from "next/navigation";
 
-const Page = async () => {
-  const { userId } = auth();
+const AskQuestion = async () => {
+  // const { userId } = auth();
 
-  if (!userId) redirect("/sign-in");
+  const userId = "12345";
+
+  if (!userId) redirect("/sing-in");
 
   const mongoUser = await getUserById({ userId });
-
+  console.log(mongoUser);
   return (
     <div>
-      <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
-
+      <h1 className="h1-bold text-dark100_light900">Ask a Question</h1>
       <div className="mt-9">
         <Question mongoUserId={JSON.stringify(mongoUser._id)} />
       </div>
@@ -22,4 +23,4 @@ const Page = async () => {
   );
 };
 
-export default Page;
+export default AskQuestion;
