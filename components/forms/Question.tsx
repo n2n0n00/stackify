81f7f74e22a887debed4915f1eddba9da91eed7e
AT <athinat690@gmail.com>
2023-10-23 13:16:53 +0100

    tinyMCE Errors

--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -1,3 +1,5 @@
+// tinyMCE Errors don't allow questions post or edit will solve later.
+
 "use client";
 import React, { useRef, useState } from "react";
 import { Editor } from "@tinymce/tinymce-react";
