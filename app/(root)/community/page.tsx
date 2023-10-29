b9d0d1d1e98de716407d1855080f73c90f435187
AT <athinat690@gmail.com>
2023-10-29 14:26:18 +0000

    localsearch: vercel couldn't resolve

--- app/(root)/community/page.tsx
+++ app/(root)/community/page.tsx
@@ -1,7 +1,7 @@
 import UserCard from "@/components/cards/UserCard";
 import Filter from "@/components/shared/Filter";
 import Pagination from "@/components/shared/Pagination";
-import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
+import LocalSearchbar from "../../../components/shared/search/LocalSearchbar";
 import { UserFilters } from "@/constants/filters";
 import { getAllUsers } from "@/lib/actions/user.action";
 import { SearchParamsProps } from "@/types";
